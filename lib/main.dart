2c5242d64abedf06eab0f95d2ee62fe31aed0eef
rebecca burch <rebecca.burch.stu@gmail.com>
2021-04-07 22:59:14 -0500

    starting to convert main flutter app to bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
@@ -8,8 +11,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   tz.initializeTimeZones();
   await Firebase.initializeApp();
+  EquatableConfig.stringify = kDebugMode;
 
-  runApp(App());
+  runApp(App(authenticationRepository: AuthenticationRepository()));
 }
 
 //https://www.youtube.com/watch?v=p5dkB3Mrxdo stateful widgets
